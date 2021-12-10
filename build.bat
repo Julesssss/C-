dd0e32b8a64f67832aae28da32b2fd7992b33af2
Julian Rosser <julianrosser91@gmail.com>
2021-12-10 00:20:00 +0000

    create batch file for build management

---
+++ build.bat
@@ -0,0 +1,18 @@
+﻿REM Disable printing
+@echo off
+
+REM Create binary dirs
+IF NOT EXIST bin mkdir bin
+IF NOT EXIST bin-int mkdir bin-int
+
+REM Change dir 
+pushd bin-int
+
+REM Compile program
+cl ..\main.cpp
+
+REM Exit directory 
+popd
+
+REM Move binary file to dir
+move bin-int\main.exe bin\main.exe
